a6c119b94e5ac86739606becb9e1fcc0c2d3b292
code-mobile <v.amelchenkov@gmail.com>
2018-05-04 22:02:59 +0300

    added second task

--- Module 1/task_1.js
+++ Module 1/task_1.js
@@ -4,3 +4,4 @@
 let str = 'hgcfgujk';
 alert(str[0]); // get the 'h'
 alert(str[3]); // get the 'f'
+
---
+++ Module 1/task_2.js
@@ -0,0 +1,8 @@
+//We have two strings, make a new one to 
+//concatenate them and get the string 'house'.
+
+
+let a = 'ho';
+let b = 'use';
+
+let c = a + b;
