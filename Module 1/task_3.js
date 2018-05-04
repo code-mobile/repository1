3691b749c948022a2cfdbcdd4c63ef23815dd61f
code-mobile <v.amelchenkov@gmail.com>
2018-05-04 22:06:07 +0300

    added third task

---
+++ Module 1/task_3.js
@@ -0,0 +1,10 @@
+//We have an array with elements 'Hello', and ' world!'.
+//The task is to put into variable hi the phrase
+// 'Hello world!', and after that show the content of the
+// variable hi.
+
+
+//solution:
+let array = ['Hello', ' world!'];
+let hi = array[0] + array[1]; 
+alert(hi);
