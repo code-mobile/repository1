4e2c77f1d0fbef4b494942f86d260eb42de3b741
code-mobile <v.amelchenkov@gmail.com>
2018-05-09 21:44:01 +0300

    added a task

---
+++ Module 1/task_4.js
@@ -0,0 +1,9 @@
+//We have an array, have to change its first element and show new 
+//array's elements as a string.
+
+//Solution:
+
+
+let r = ['Hello' , ' world'];
+r[0] = 'Hi'; //change the first element
+console.log(r[0] + r[1]);
