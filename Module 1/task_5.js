cca17f88a6b88b41aac28866823437e0431c2e49
code-mobile <v.amelchenkov@gmail.com>
2018-05-11 21:52:04 +0300

    added a new task

---
+++ Module 1/task_5.js
@@ -0,0 +1,13 @@
+//In this task you're supposed to get even numbers starting 
+//from 2 to 10.
+
+
+
+//One of possible solutions:
+
+
+for (var i = 2; i < 11; i = i + 2) {
+    console.log(i);
+    
+}
+  
